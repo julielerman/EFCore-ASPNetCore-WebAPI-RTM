404e9c430781328a2ab39e0ab390bf0d48eca921
julie <julielerman@gmail.com>
2016-01-16 09:56:43 -0500

    Fix WeatherEvent
    added a parameterless ctor (still needed in EF7..makes sense) and on
    overloaded ctor, made sure it set value of hooray

--- Models/WeatherEvent.cs
+++ Models/WeatherEvent.cs
@@ -2,32 +2,35 @@ using System;
 
 namespace EF7WebAPI
 {
-    public class WeatherEvent
-    {
-        public static WeatherEvent Create(DateTime date, WeatherType type, Boolean hooray)
-        {
-            return new WeatherEvent(date, type, hooray);
-        }
-        private WeatherEvent(DateTime dateTime, WeatherType type, Boolean hooray) { 
-            this.Date=dateTime.Date;
-            this.Time=dateTime.TimeOfDay;
-            Hooray=hooray;
-        }
-        public int Id { get; set; }
-        public DateTime Date { get; private set; }
-
-        public TimeSpan Time { get; private set; }
-
-        public WeatherType Type { get; private set; }
-
-        public bool Hooray { get; private set; }
+  public class WeatherEvent
+  {
+    public static WeatherEvent Create(DateTime date, WeatherType type, Boolean hooray) {
+      return new WeatherEvent(date, type, hooray);
     }
-    public enum WeatherType
-    {
-        Rain = 1,
-        Snow = 2,
-        Sleet = 3,
-        Hail = 4,
-        Sun = 5
+    public WeatherEvent() {
+
     }
+    private WeatherEvent(DateTime dateTime, WeatherType type, Boolean hooray) {
+      this.Date = dateTime.Date;
+      this.Time = dateTime.TimeOfDay;
+      Hooray = hooray;
+      Type = type;
+    }
+    public int Id { get; set; }
+    public DateTime Date { get; private set; }
+
+    public TimeSpan Time { get; private set; }
+
+    public WeatherType Type { get; private set; }
+    
+    public bool Hooray { get; private set; }
+  }
+  public enum WeatherType
+  {
+    Rain = 1,
+    Snow = 2,
+    Sleet = 3,
+    Hail = 4,
+    Sun = 5
+  }
 }

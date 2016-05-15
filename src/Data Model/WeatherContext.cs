52106a7bb0dd5bc493f6add6b985da06d3c94570
julie <julielerman@gmail.com>
2016-05-16 00:52:06 +0200

    not sure where I left this oops

--- src/Data Model/WeatherContext.cs
+++ src/Data Model/WeatherContext.cs
@@ -17,5 +17,13 @@ namespace EFCoreWebAPI.Data
             : base(serviceProvider, options)
         { }
         public DbSet<WeatherEvent> WeatherEvents { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        //quick and dirty takes care of my entities not all scenarios
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            modelBuilder.Entity(entity.Name).ToTable(entity.ClrType.Name + "s");
+        }
+        }
     }
 }

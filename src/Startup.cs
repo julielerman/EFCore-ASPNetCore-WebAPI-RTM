8775b3feaded626b133dfba5227fa6e9687f81f9
julie <julielerman@gmail.com>
2016-05-17 20:32:14 -0400

    adding setbasepath to startup , replacing loggin verbose with Debug (verbose no longer exists), fixing program path in launch.json ...I had it all wrong

--- src/Startup.cs
+++ src/Startup.cs
@@ -36,6 +36,7 @@ namespace EFCoreWebAPI
 
             // Set up configuration sources.
             var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
             Configuration = builder.Build();

27ae8d085f6dd04cdbab15defcab8e1964451d09
julie <julielerman@gmail.com>
2016-05-18 14:33:40 -0400

    clean startup

--- src/Startup.cs
+++ src/Startup.cs
@@ -33,40 +33,25 @@ namespace EFCoreWebAPI
 
         public Startup(IHostingEnvironment env)
         {
-
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder()
+          var builder = new ConfigurationBuilder()
                .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
-             _platform=new Platform();
-            var platString= _platform.ToString();
-             
- var _isMono = string.Equals(
-                        PlatformServices.Default.Runtime.RuntimeType,
-                        "Mono",
-                        StringComparison.OrdinalIgnoreCase);
-       
         }
 
         public IConfigurationRoot Configuration { get; set; }
 
-         public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-                services.AddMvc();
-
-            //note: see https://github.com/AspNetCore/MusicStore/blob/dev/src/MusicStore/Startup.cs
-            //for post RC1 implementation of determining if this is a test
-            //with the platform inspection
-
+            services.AddMvc();
 
             services.AddDbContext<WeatherContext>(options =>
               options.UseNpgsql(Configuration["Data:PostgreConnection:ConnectionString"]));
             services.AddScoped<InternalServices>();
         }
 
-          public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();

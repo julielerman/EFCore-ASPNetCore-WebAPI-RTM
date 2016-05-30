b74a249efc18340451af06059cf19c48ae04e662
julie <julielerman@gmail.com>
2016-05-29 20:53:04 -0400

    fixed up new controller method to demonstrate use of trackgraph

--- src/Controllers/WeatherController.cs
+++ src/Controllers/WeatherController.cs
@@ -46,24 +46,28 @@ namespace EFCoreWebAPI.Controllers
         {
             //return _context.WeatherEvents.FromSql($"SELECT * FROM EventsByType({weatherType})").OrderByDescending(e => e.Id);
             // return _context.WeatherEvents.Where(w => w.Type==WeatherType.Rain).ToList();
-             return _context.WeatherEvents.Where(w => (int)w.Type==weatherType).ToList();
+            return _context.WeatherEvents.Where(w => (int)w.Type == weatherType).ToList();
         }
 
         [HttpPost]
         public int LogWeatherEvent(DateTime datetime, WeatherType type, bool happy,
                                         string name, string quote)
         {
+            
+            //This controller method demonstrates that DbSet Add behaves the same
+            //as EF6: affecting every item in a [disconnected] graph with the same state
+            //As well as Attach, Remove and the new Update.
+            
             WeatherEvent wE;
             if (String.IsNullOrEmpty(name))
             {
                 wE = WeatherEvent.Create(datetime, type, happy);
-             }
+            }
             else
             {
                 wE = WeatherEvent.Create(datetime, type, happy,
                                          new List<string[]> { new[] { name, quote } });
-             }
-            //*Add, Attach, Update, Remove affects all items in graph
+            }
              _context.WeatherEvents.Add(wE);
             var affectedRowCount = _context.SaveChanges();
             return affectedRowCount;
@@ -72,6 +76,8 @@ namespace EFCoreWebAPI.Controllers
         [HttpPut("{eventId}")]
         public string GetAndUpdateMostCommonWord(int eventId)
         {
+            //This contoller method demonstrates the NEW BEHAVIOR of DbContext.Entry.State
+            //which will only affect the root of the graph passed in
             var eventGraph = _repo.GetWeatherEventAndReactionsById(eventId);
             var theWord = ReactionParser.MostFrequentWord(
                 eventGraph.Reactions.Select(r => r.Quote).ToList());
@@ -80,27 +86,34 @@ namespace EFCoreWebAPI.Controllers
             Console.WriteLine($"NOTE: Graph still has {eventGraph.Reactions.Count} reactions attached");
             return theWord;
         }
-        
+
         [HttpPutAttribute("clean")]
-        public string FixSeasonInReactions(){
-           var eventGraphs=_repo.GetAllEventsWithReactions();
-           
-           foreach (Reaction reaction in eventGraphs.Where(e=>e.Date.Month<5).Select(e=>e.Reactions))
-           {
-              if (reaction.Quote.ToUpper().Contains("SNOW"))
-              {
-                  reaction.Quote.ToUpper().Replace("SNOW", "sun");
-                  reaction.ObjectState=ObjectState.Modified;
-              }
-              
-           }
-           var affectedRecords=_repo.UpdateRandomStateWeatherEventGraphs(eventGraphs);
-          return affectedRecords.ToString();
-           //if date of event is after May 1, then look for snow in reation and change to "sun"
-           //pas graphs to updater, use trackchange to update only the modified entries
-           //need a few more reactions to show some with "snow" an some without
-           //also need to add localstate property to weather event and to reaction.
-        }
+        public string FixSeasonInReactions()
+        { 
+             //This contoller method demonstrates the NEW DbContext.ChangeTracker.TrackGraph method
+             //(in the repo method that's called from here)
+            //which will iterate through all items in a graph and apply the specified 
+            //function each item. 
+   
+            int totalDbRowsUpdated = 0;
+            var eventGraphs = _repo.GetAllEventsWithReactions();
+            foreach (var graph in eventGraphs.Where(e => e.Date.Month > 4))
+            {
+                foreach (var reaction in graph.Reactions)
+
+                {
+                    if (reaction.Quote.ToUpper().Contains("SNOW"))
+                    {
+                        reaction.Quote = reaction.Quote.ToUpper().Replace("SNOW", "sun");
+                        reaction.ObjectState = ObjectState.Modified;
+                    }
+
+                }
+                var affectedRecords = _repo.UpdateRandomStateWeatherEventGraphs(eventGraphs);
+                totalDbRowsUpdated += affectedRecords;
+            }
+            return totalDbRowsUpdated.ToString();
+             }
 
 
 
